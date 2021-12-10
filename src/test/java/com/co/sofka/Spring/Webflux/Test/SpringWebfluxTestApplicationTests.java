7733a9b0a3bb96982bf070c0c3b33a41774d53db
Antonio Alzate <antonioalzate7@gmail.com>
2021-12-09 21:17:31 -0500

    Commit inicial.

---
+++ src/test/java/com/co/sofka/Spring/Webflux/Test/SpringWebfluxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.Spring.Webflux.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
