ec9d3cd05894f997a316c9da74ab52b626bbc773
Antonio Alzate <antonioalzate7@gmail.com>
2021-12-09 21:23:39 -0500

    Se realiza TestPublisher

---
+++ src/main/java/com/co/sofka/Spring/Webflux/Test/TestPublisher/UppercaseConverter.java
@@ -0,0 +1,16 @@
+package com.co.sofka.Spring.Webflux.Test.TestPublisher;
+
+import reactor.core.publisher.Flux;
+
+class UppercaseConverter {
+    private final Flux<String> source;
+
+    UppercaseConverter(Flux<String> source) {
+        this.source = source;
+    }
+
+    Flux<String> getUpperCase() {
+        return source
+                .map(String::toUpperCase);
+    }
+}
